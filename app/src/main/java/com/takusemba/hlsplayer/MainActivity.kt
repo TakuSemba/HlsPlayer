efc5afd03884cd7e3651661b97ec0e76646901b8
takusemba <taku1008ele@gmail.com>
2018-02-03 16:07:03 +0900

    retry added

--- app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
+++ app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
@@ -4,9 +4,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.DefaultRenderersFactory
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
@@ -15,12 +17,21 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 class MainActivity : AppCompatActivity() {
 
     private val handler = Handler()
+    private var player: SimpleExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val player = ExoPlayerFactory.newSimpleInstance(
+        play()
+
+        findViewById<Button>(R.id.retry).setOnClickListener {
+            retry()
+        }
+    }
+
+    private fun play() {
+        player = ExoPlayerFactory.newSimpleInstance(
                 DefaultRenderersFactory(this), DefaultTrackSelector(), DefaultLoadControl())
         val playerView = findViewById<SimpleExoPlayerView>(R.id.player_view)
         val uri = Uri.parse(BuildConfig.STREAMING_URL)
@@ -30,7 +41,17 @@ class MainActivity : AppCompatActivity() {
         val dataSourceFactory = DefaultDataSourceFactory(this, "user-agent")
         val mediaSource = HlsMediaSource(uri, dataSourceFactory, handler, null)
 
-        player.prepare(mediaSource)
-        player.playWhenReady = true
+        player?.prepare(mediaSource)
+        player?.playWhenReady = true
+    }
+
+    private fun stop() {
+        player?.playWhenReady = false
+        player?.release()
+    }
+
+    private fun retry() {
+        stop()
+        play()
     }
 }
