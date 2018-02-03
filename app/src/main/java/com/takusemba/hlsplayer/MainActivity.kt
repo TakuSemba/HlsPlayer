a185926608c4885106a9cd8aaf869809acf3f2c0
takusemba <taku1008ele@gmail.com>
2018-02-03 15:56:53 +0900

    play hls

--- app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
+++ app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.takusemba.hlsplayer
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val player = ExoPlayerFactory.newSimpleInstance(
+                DefaultRenderersFactory(this), DefaultTrackSelector(), DefaultLoadControl())
+        val playerView = findViewById<SimpleExoPlayerView>(R.id.player_view)
+        val uri = Uri.parse(BuildConfig.STREAMING_URL)
+
+        playerView.player = player
+
+        val dataSourceFactory = DefaultDataSourceFactory(this, "user-agent")
+        val mediaSource = HlsMediaSource(uri, dataSourceFactory, handler, null)
+
+        player.prepare(mediaSource)
+        player.playWhenReady = true
     }
 }
